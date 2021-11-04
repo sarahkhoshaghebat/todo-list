9e026093548252e23699d23e3f2c1d921f07ffed
sara <sarah_khoshaghebat@yahoo.com>
2021-11-04 15:04:25 +0330

    redesign

--- src/main.js
+++ src/main.js
@@ -88,6 +88,7 @@ handleInputChange= (event) => {
                                         <span className="icon_add" aria-hidden="true">
                                         <svg width="13" height="13"><path d="M6 6V.5a.5.5 0 0 1 1 0V6h5.5a.5.5 0 1 1 0 1H7v5.5a.5.5 0 1 1-1 0V7H.5a.5.5 0 0 1 0-1H6z" fill="currentColor" fillRule="evenodd"></path></svg></span>
                                     </button>
+                                    <p>Add task</p>
                                 </div>
                             </div>
                         </div>
