caee1f7971edb3358f09f95d8f53d0919e9f166a
sara <sarah_khoshaghebat@yahoo.com>
2021-11-05 17:21:31 +0330

    redesign

--- src/main.js
+++ src/main.js
@@ -84,11 +84,11 @@ handleInputChange= (event) => {
                             </div>
                             <div className="row">
                                 <div className="col">
-                                    <button>
+                                    <a href="/#">
                                         <span className="icon_add" aria-hidden="true">
                                         <svg width="13" height="13"><path d="M6 6V.5a.5.5 0 0 1 1 0V6h5.5a.5.5 0 1 1 0 1H7v5.5a.5.5 0 1 1-1 0V7H.5a.5.5 0 0 1 0-1H6z" fill="currentColor" fillRule="evenodd"></path></svg></span>
-                                    </button>
-                                    <p>Add task</p>
+                                        <span>Add task</span>
+                                    </a>
                                 </div>
                             </div>
                         </div>
