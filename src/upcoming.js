ad23830c7543481b1d1f53bac87fb65df6ca1998
sara <sarah_khoshaghebat@yahoo.com>
2021-11-11 21:53:49 +0330

    add file

---
+++ src/upcoming.js
@@ -0,0 +1,16 @@
+import  React,{ Component } from "react";
+import './App.css';
+
+class Upcoming extends Component {
+    render() {
+        return (
+            <>
+                <div>
+                    <input type="deta"/>
+                </div>
+            </>
+        )
+    }
+}
+
+export default Upcoming;
